f9833c5c76a5aaf6e4e2297afec39540c64cee24
Tmosco <you@example.com>
2019-05-15 10:24:48 +0100

    the user.js

--- api/route/user.js
+++ api/route/user.js
@@ -28,6 +28,17 @@ isAdmin : false ,
 
 ];
 
+
+function validateUser(user){
+    const schema = {
+        // email: Joi.string().email({ minDomainAtoms: 2 }).required(),
+        firstname:Joi.string(),
+        lastname:Joi.string(),
+        // isAdmin:Joi.boolean()
+    }
+    return Joi.validate(user,schema);
+}
+
 router.post('/', (req, res, next) => {
     const schema = {
         email: Joi.string().email({ minDomainAtoms: 2 }).required(),
@@ -57,7 +68,7 @@ router.post('/', (req, res, next) => {
       res.status(201).json({
     
         message: 'User was created',
-        createdParty: user,
+        createdUser: user,
       });
     }
     });
@@ -84,11 +95,44 @@ router.get('/:id', (req, res, next) => {
 
 router.patch('/:id', (req, res, next) => {
     const user = users.find(user => user.id === parseInt(req.params.id));
+    // const result = validateUser(req.body);
+    const  {error} = validateUser(req.body);
+    
+  
+  if (error) {
+    res.status(404).json({
+      error:error.details[0].message,
+    });
+  }
+  else{
+//I should be able to decide if i only want to use firstname
+      user.firstname = req.body.firstname;
+      res.status(200).json({
+          user,
+        });
+    }
 });
 
-router.delete('/:id', (req, res, next) => {
 
+
+
+router.delete('/:id', (req, res, next) => {
+    const user = users.find(user => user.id === parseInt(req.params.id));
+    if (!user) {
+        res.status(404).json({
+          message: 'User is not available',
+        });
+      }
+      const index = users.indexOf(user);
+      users.splice(index, 1);
+    
+      res.status(200).json({
+        message: 'User has been deleted',
+      });
 });
 
 
 module.exports = router;
+
+
+
